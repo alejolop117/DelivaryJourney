74528d3ee1140226cebdc11b55f5189fcf5990c6
estebanmont <88239099+estebanmont@users.noreply.github.com>
2022-04-27 21:37:30 -0500

    Skins In game (#35)
    skins jugables

--- Assets/GameOverTuto.cs
+++ Assets/GameOverTuto.cs
@@ -13,7 +13,7 @@ public class GameOverTuto : MonoBehaviour
     public void CargarTuto()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("TutorialTesteo");
+        SceneManager.LoadScene("Tutorial");
 
     }
     public void CargarMenu()
