c1cd299ff1cb82756391e4fe04035780c52f7374
Alejo Lopez <alejandro.lopezp@upb.edu.co>
2022-05-08 20:36:47 -0500

    ScriptDerrape

--- Assets/Scripts/Player/AnimationManager.cs
+++ Assets/Scripts/Player/AnimationManager.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    private void Awake() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
